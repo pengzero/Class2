0ffb19a6e2bc13b0c8906e5d8ca84e9682d49a7d
Zhantao Peng <peng.z@husky.neu.edu>
2016-01-31 21:14:28 -0500

    first draft
    still try to add more stuff. like when mousepressed, a random bouncing
    ball will show up.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
-function setup () {
-  createCanvas(600,600);
+var x = 0;
+var speed = 20;
+function setup() {
+  createCanvas (600,600)
+  
 }
 
 function draw() {
+  background(255)
 
+    for (var y=0; y < height; y+=20){
+      new bouncingball(x,y,20)
+    }
+    if(x > width){
+      speed = -20
+    }
+    x = x + speed
+    if (mouseIsPressed)
+    speed = 0
+  }
+
+
+var bouncingball = function(x,y,ellipseSize){
+  var colors = color(random(0,255),random(0,255),random(100,255));
+  fill(colors)
+  line(x,y,mouseX,mouseY)
+  ellipse(x,y,ellipseSize,ellipseSize)
 }
+
